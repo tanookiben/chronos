d2ad701b8f5ad7c83f0bb4a34539b3f20f36675f
Benjamin Hsieh <ben@benspotatoes.com>
2017-05-04 22:46:27 -0700

    Remove broken test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,8 +1,3 @@
 defmodule Chronos.PageControllerTest do
   use Chronos.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
 end
