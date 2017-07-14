cc9f61ac56646a3583d4d90ddb3ba228d114949c
Benjamin Hsieh <ben@benspotatoes.com>
2017-07-13 22:37:35 -0700

    Update photos controller

--- web/controllers/photo_controller.ex
+++ web/controllers/photo_controller.ex
@@ -1,12 +1,9 @@
 defmodule Chronos.PhotoController do
   use Chronos.Web, :controller
 
-  alias Chronos.Photo
+  plug Guardian.Plug.EnsureAuthenticated, handler: Chronos.AuthHandler
 
-  def index(conn, _params) do
-    photos = Repo.all(Photo)
-    render(conn, "index.html", photos: photos)
-  end
+  alias Chronos.Photo
 
   def new(conn, _params) do
     changeset = Photo.changeset(%Photo{})
@@ -14,41 +11,37 @@ defmodule Chronos.PhotoController do
   end
 
   def create(conn, %{"photo" => photo_params}) do
-    changeset = Photo.changeset(%Photo{}, photo_params)
-
-    case Repo.insert(changeset) do
-      {:ok, _photo} ->
+    upload = photo_params["upload"]
+    case File.read(upload.path) do
+      {:ok, bin} ->
+        path = "photos/#{UUID.uuid4(:hex)}#{Path.extname(upload.filename)}"
+        case ExAws.S3.put_object("chronos-timeline", path, bin, acl: :public_read) |> ExAws.request() do
+          {:ok, _} ->
+            photo = %Photo{uploader: Guardian.Plug.current_resource(conn).email, s3_path: path}
+            changeset = Photo.changeset(photo, photo_params)
+            case Repo.insert(changeset) do
+              {:ok, _photo} ->
+                conn
+                |> put_flash(:info, "Photo created successfully.")
+                |> redirect(to: event_path(conn, :index))
+              {:error, changeset} ->
+                render(conn, "new.html", changeset: changeset)
+            end
+          _ ->
+            conn
+            |> put_flash(:error, "Unable to put object to S3")
+            |> render("new.html", changeset: Photo.changeset(%Photo{}))
+        end
+      _ ->
         conn
-        |> put_flash(:info, "Photo created successfully.")
-        |> redirect(to: photo_path(conn, :index))
-      {:error, changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        |> put_flash(:error, "Unable to read file")
+        |> render("new.html", changeset: Photo.changeset(%Photo{}))
     end
   end
 
   def show(conn, %{"id" => id}) do
     photo = Repo.get!(Photo, id)
     render(conn, "show.html", photo: photo)
-  end
-
-  def edit(conn, %{"id" => id}) do
-    photo = Repo.get!(Photo, id)
-    changeset = Photo.changeset(photo)
-    render(conn, "edit.html", photo: photo, changeset: changeset)
-  end
-
-  def update(conn, %{"id" => id, "photo" => photo_params}) do
-    photo = Repo.get!(Photo, id)
-    changeset = Photo.changeset(photo, photo_params)
-
-    case Repo.update(changeset) do
-      {:ok, photo} ->
-        conn
-        |> put_flash(:info, "Photo updated successfully.")
-        |> redirect(to: photo_path(conn, :show, photo))
-      {:error, changeset} ->
-        render(conn, "edit.html", photo: photo, changeset: changeset)
-    end
   end
 
   def delete(conn, %{"id" => id}) do
@@ -60,6 +53,6 @@ defmodule Chronos.PhotoController do
 
     conn
     |> put_flash(:info, "Photo deleted successfully.")
-    |> redirect(to: photo_path(conn, :index))
+    |> redirect(to: event_path(conn, :index))
   end
 end
